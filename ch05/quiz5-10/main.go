fc917edf3b9eda5d9c6325f1c4c949b152b307b7
soeyusuke <s1240231@gmail.com>
2017-10-23 14:05:47 +0900

    [ADD] quiz5-10 練習問題

---
+++ ch05/quiz5-10/main.go
@@ -0,0 +1,58 @@
+/*
+スライスの代わりにmapを使うようにtopoSortを書き直して,最初のソートを削除しなさい。
+結果は否定的ですが、結果が有効なトポロジカル順序になっていることを検証しなさい
+*/
+package main
+
+import (
+	"fmt"
+)
+
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	visitAll(keys)
+	return order
+}
